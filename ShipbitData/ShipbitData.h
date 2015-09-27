3e915de7aed91fbd58a9b623b16e3aa13f83e63a
Patrick Mick <patrickmick1@gmail.com>
2015-09-26 21:26:28 -0700

    Updating app icons

---
+++ ShipbitData/ShipbitData.h
@@ -0,0 +1,19 @@
+//
+//  ShipbitData.h
+//  ShipbitData
+//
+//  Created by Patrick Mick on 9/26/15.
+//  Copyright © 2015 Patrick Mick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShipbitData.
+FOUNDATION_EXPORT double ShipbitDataVersionNumber;
+
+//! Project version string for ShipbitData.
+FOUNDATION_EXPORT const unsigned char ShipbitDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShipbitData/PublicHeader.h>
+
+
