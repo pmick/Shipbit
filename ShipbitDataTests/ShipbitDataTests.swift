612b9282558225af7e387d422ec15b76ae14d6fd
Patrick Mick <patrickmick1@gmail.com>
2015-09-26 23:12:55 -0700

    Revert "Removing the default test files as one is causing an error"
    This reverts commit 250a5704e6d8584f3e51627798ecc78e715a4526.

---
+++ ShipbitDataTests/ShipbitDataTests.swift
@@ -0,0 +1,33 @@
+//
+//  ShipbitDataTests.swift
+//  ShipbitDataTests
+//
+//  Created by Patrick Mick on 9/26/15.
+//  Copyright © 2015 Patrick Mick. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import Quick
+@testable import ShipbitData
+
+class ShipbitDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        expect(1).to(equal(1))
+    }
+    
+    
+}
