715772b47e14cc73f441453c6b2b4296efeb5aad
Patrick Mick <patrickmick1@gmail.com>
2015-09-26 22:44:23 -0700

    Removing time measurement test as it was causing tests to indicate failure on travis.

--- ShipbitDataTests/ShipbitDataTests.swift
+++ ShipbitDataTests/ShipbitDataTests.swift
@@ -29,11 +29,5 @@ class ShipbitDataTests: XCTestCase {
         expect(1).to(equal(1))
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
